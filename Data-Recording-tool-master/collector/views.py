5d549debfa7c1b88affa43240174081698f922c8
nayanjha16 <55428109+nayanjha16@users.noreply.github.com>
2022-07-21 10:25:40 +0530

    Update views.py

--- Data-Recording-tool-master/collector/views.py
+++ Data-Recording-tool-master/collector/views.py
@@ -8,7 +8,11 @@ def dashboard(request):
         try:
             user_prompts = models.audio_files.objects.filter(user=request.user)
             last_prompt = user_prompts.last()
-            if last_prompt.sentence.id < models.sentences.objects.count():
+            if last_prompt.audio.name[-5]=="i":
+                new_prompt_id = last_prompt.sentence.id
+                new_prompt = models.sentences.objects.get(id=new_prompt_id)
+                
+            elif last_prompt.sentence.id < models.sentences.objects.count():
                 new_prompt_id = last_prompt.sentence.id + 1
                 new_prompt = models.sentences.objects.get(id=new_prompt_id)
             else:
@@ -28,13 +32,15 @@ def dashboard(request):
 def upload_audio(request):
     if request.method == 'POST':
         audio_upload = forms.audio_upload_form(request.POST, request.FILES)
+        num= int(request.POST['num'])
         if 'save_all' in request.POST:
             if audio_upload.is_valid():
                 audio = audio_upload.save(commit=False)
-                #prompt = models.sentences.objects.get(id=request.POST['pid'])
-                #audio.sentence = prompt
+                prompt = models.sentences.objects.get(id=request.POST['pid'])
+                
+                audio.sentence = prompt
                 audio.user = request.user
-                audio.audio.name = str(audio.user.id) + "_" + str(audio.sentence.id) +num+ ".wav"
+                audio.audio.name = str(audio.user.id) + "_" + str(audio.sentence.id) +num+ "i.wav"
                 audio.save()
         else:
             if audio_upload.is_valid():
